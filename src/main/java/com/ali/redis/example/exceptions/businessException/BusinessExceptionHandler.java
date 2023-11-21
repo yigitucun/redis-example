f1c42ed85c148105693dc38c9ab4c367aa10a1c2
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2023-11-21 15:38:22 +0300

    Update BusinessExceptionHandler.java

--- src/main/java/com/ali/redis/example/exceptions/businessException/BusinessExceptionHandler.java
+++ src/main/java/com/ali/redis/example/exceptions/businessException/BusinessExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.ali.redis.example.exceptions.businessException;
 
 
-import com.ali.authenticationservice.exceptions.ExceptionDetails;
+import com.ali.redis.example.exceptions.ExceptionDetails;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
