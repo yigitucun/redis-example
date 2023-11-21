9a00cf5507041cac13e3702331ea6a8ff5a66d55
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2023-11-21 15:52:24 +0300

    Update CityManager.java

--- src/main/java/com/ali/redis/example/business/concretes/CityManager.java
+++ src/main/java/com/ali/redis/example/business/concretes/CityManager.java
@@ -1,4 +1,26 @@
 package com.ali.redis.example.business.concretes;
 
-public class CityManager {
+import com.ali.redis.example.business.abstracts.CityService;
+import com.ali.redis.example.dataAccess.abstracts.ICityRepository;
+import com.ali.redis.example.dto.requests.AddCityRequest;
+import com.ali.redis.example.dto.responses.ListCitiesResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class CityManager implements CityService {
+
+    private ICityRepository cityRepository;
+    @Override
+    public AddCityRequest add(AddCityRequest request) {
+        return null;
+    }
+
+    @Override
+    public List<ListCitiesResponse> getAllCities() {
+        return null;
+    }
 }
