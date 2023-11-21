d1d87c6d5b3068d5bc666653144a42e10d57b691
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2023-11-21 22:15:48 +0300

    Update ICityRepository.java

--- src/main/java/com/ali/redis/example/dataAccess/abstracts/ICityRepository.java
+++ src/main/java/com/ali/redis/example/dataAccess/abstracts/ICityRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface ICityRepository extends JpaRepository<City,Integer> {
     boolean existsByPlateCode(int id);
     boolean existsByCity(String city);
+    boolean existsById(int id);
 }
