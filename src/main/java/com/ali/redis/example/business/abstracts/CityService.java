3df0d7f4c023d4737e2ca9afbee7a4fd2ad2b4cb
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2023-11-21 15:52:22 +0300

    Update CityService.java

--- src/main/java/com/ali/redis/example/business/abstracts/CityService.java
+++ src/main/java/com/ali/redis/example/business/abstracts/CityService.java
@@ -1,4 +1,11 @@
 package com.ali.redis.example.business.abstracts;
 
+import com.ali.redis.example.dto.requests.AddCityRequest;
+import com.ali.redis.example.dto.responses.ListCitiesResponse;
+
+import java.util.List;
+
 public interface CityService {
+    AddCityRequest add(AddCityRequest request);
+    List<ListCitiesResponse> getAllCities();
 }
