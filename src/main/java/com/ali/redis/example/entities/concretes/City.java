3c8c28464b56547769a9fb005c1111816cfb4e3e
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2023-11-21 15:38:20 +0300

    Update City.java

--- src/main/java/com/ali/redis/example/entities/concretes/City.java
+++ src/main/java/com/ali/redis/example/entities/concretes/City.java
@@ -1,4 +1,21 @@
 package com.ali.redis.example.entities.concretes;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cities")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String city;
+    private int plateCode;
 }
