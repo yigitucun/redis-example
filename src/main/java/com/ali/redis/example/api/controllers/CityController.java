fd679ea9e688b8f96465b1b65c0fe20e93adb460
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2023-11-21 18:26:29 +0300

    Update CityController.java

--- src/main/java/com/ali/redis/example/api/controllers/CityController.java
+++ src/main/java/com/ali/redis/example/api/controllers/CityController.java
@@ -5,9 +5,7 @@ import com.ali.redis.example.dto.requests.AddCityRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/cities")
@@ -16,7 +14,13 @@ public class CityController {
 
     private CityService cityService;
 
+    @PostMapping
     public ResponseEntity<?> add(@RequestBody AddCityRequest request){
         return new ResponseEntity<>(this.cityService.add(request), HttpStatus.CREATED);
     }
+    @GetMapping
+    public ResponseEntity<?> getAllCities(){
+        return ResponseEntity.ok(this.cityService.getAllCities());
+    }
+
 }
