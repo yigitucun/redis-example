fcd4f1d82be630c1ba40cf3e8af8b7bae20a1c20
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2023-11-21 18:26:36 +0300

    Create RedisConfig.java

---
+++ src/main/java/com/ali/redis/example/configs/RedisConfig.java
@@ -0,0 +1,25 @@
+package com.ali.redis.example.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+import java.util.HashMap;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+    @Bean
+    public LettuceConnectionFactory connectionFactory(){
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<?,?> template(){
+        RedisTemplate<byte[],byte[]> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory());
+        return template;
+    }
+}
