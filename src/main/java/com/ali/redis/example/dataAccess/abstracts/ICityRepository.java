132c6af8c45ccd0a3ca79b30f73d24acec3d688f
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2023-11-21 15:48:03 +0300

    Create ICityRepository.java

---
+++ src/main/java/com/ali/redis/example/dataAccess/abstracts/ICityRepository.java
@@ -0,0 +1,9 @@
+package com.ali.redis.example.dataAccess.abstracts;
+
+import com.ali.redis.example.entities.concretes.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICityRepository extends JpaRepository<City,Integer> {
+}
