d5681c566346b3aaf0c95c70b30026725bbabfa7
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2023-11-21 22:36:35 +0300

    Update UpdateCityRequest.java

--- src/main/java/com/ali/redis/example/dto/requests/UpdateCityRequest.java
+++ src/main/java/com/ali/redis/example/dto/requests/UpdateCityRequest.java
@@ -3,14 +3,17 @@ package com.ali.redis.example.dto.requests;
 import com.ali.redis.example.entities.concretes.City;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class UpdateCityRequest {
-    
+public class UpdateCityRequest implements Serializable {
+
     private int id;
     private String city;
     private int plateCode;
     public City toEntity(UpdateCityRequest request){
         return City.builder()
+                .id(request.getId())
                 .plateCode(request.getPlateCode())
                 .city(request.getCity())
                 .build();
