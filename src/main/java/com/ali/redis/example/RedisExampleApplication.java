c1b686a4424d501f435ab929093f121eb43c2f06
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2023-11-21 18:26:39 +0300

    Update RedisExampleApplication.java

--- src/main/java/com/ali/redis/example/RedisExampleApplication.java
+++ src/main/java/com/ali/redis/example/RedisExampleApplication.java
@@ -2,8 +2,10 @@ package com.ali.redis.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class RedisExampleApplication {
 
 	public static void main(String[] args) {
