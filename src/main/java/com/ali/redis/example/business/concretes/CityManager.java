50a96d40fb5b424f4ab9c923dc98573d0cfa5abc
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2023-11-21 15:56:09 +0300

    Update CityManager.java

--- src/main/java/com/ali/redis/example/business/concretes/CityManager.java
+++ src/main/java/com/ali/redis/example/business/concretes/CityManager.java
@@ -1,6 +1,7 @@
 package com.ali.redis.example.business.concretes;
 
 import com.ali.redis.example.business.abstracts.CityService;
+import com.ali.redis.example.business.rules.CityBusinessRules;
 import com.ali.redis.example.dataAccess.abstracts.ICityRepository;
 import com.ali.redis.example.dto.requests.AddCityRequest;
 import com.ali.redis.example.dto.responses.ListCitiesResponse;
@@ -14,10 +15,14 @@ import java.util.List;
 public class CityManager implements CityService {
 
     private final ICityRepository cityRepository;
-    private final C
+    private final CityBusinessRules cityBusinessRules;
     @Override
     public AddCityRequest add(AddCityRequest request) {
-        return null;
+        this.cityBusinessRules.checkIfCityCity(request.getCity());
+        this.cityBusinessRules.checkIfCityPlateCode(request.getPlateCode());
+        this.cityRepository.save(request.toEntity(request));
+        return request;
+
     }
 
     @Override
