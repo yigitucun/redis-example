13d7a5456961948db9fd797ee94d1111007b33b8
Yiğit Ali Uçun <yigit_ali_ucun@outlook.com>
2023-11-21 18:26:38 +0300

    Update AddCityRequest.java

--- src/main/java/com/ali/redis/example/dto/requests/AddCityRequest.java
+++ src/main/java/com/ali/redis/example/dto/requests/AddCityRequest.java
@@ -5,9 +5,9 @@ import lombok.Data;
 
 @Data
 public class AddCityRequest {
+
     private String city;
     private int plateCode;
-
     public City toEntity(AddCityRequest request){
         return City.builder()
                 .city(request.getCity())
